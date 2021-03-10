1856ac7a84836072a76271e9a0146a8ce2e44834
eliiifyiigit <eliiifyiig@gmail.com>
2021-03-10 11:15:30 +0300

    no message

---
+++ public/assets/scripts/sweetalert.init.js
@@ -0,0 +1,151 @@
+/**
+ * Theme: Ninja Admin Template
+ * Author: NinjaTeam
+ * Module/App: Sweet Alert
+ */
+
+(function($) {
+	"use strict";
+
+	var alert = {};
+
+	$(document).ready(function(){
+		if ($("#sal-basic").length) alert.basic();
+		if ($("#sal-text").length) alert.text();
+        if ($("#sal-success").length) alert.success();
+        if ($("#sal-notsuccess").length) alert.notsuccess();
+		if ($("#sal-warning").length) alert.warning();
+		if ($("#sal-icon").length) alert.icon();
+		if ($("#sal-html").length) alert.html();
+		if ($("#sal-auto").length) alert.auto();
+		if ($("#sal-prompt").length) alert.prompt();
+		return false;
+	});
+
+	alert = {
+		basic : function(){
+			$("#sal-basic").on("click",function(){
+				swal({
+					title: "Here's a message!",
+					confirmButtonColor: '#304ffe'
+				});
+				return false;
+			});
+			return false;
+		},
+		text: function(){
+			$("#sal-text").on("click",function(){
+				swal({
+					title: "Here's a message!",
+					text: "It's pretty, isn't it?",
+					confirmButtonColor: '#304ffe'
+				});
+				return false;
+			});
+			return false;
+		},
+        success: function(){
+            $("#sal-success").on("click",function(){
+                swal({
+                    title: "Good job!",
+                    text: "You clicked the button!",
+                    type: 'success',
+                    confirmButtonColor: '#304ffe'
+                });
+                return false;
+            });
+            return false;
+        },notsuccess: function(){
+            $("#sal-notsuccess").on("click",function(){
+                swal({
+                    title: "bad job!",
+                    text: "You clicked the button!",
+                    type: 'warning',
+                    confirmButtonColor: '#304ffe'
+                });
+                return false;
+            });
+            return false;
+        },warning: function(){
+            $("#sal-warning").on("click",function(){
+                swal({
+                    title: "Good job!",
+                    text: "You clicked the button!",
+                    type: 'warning',
+                    confirmButtonColor: '#304ffe'
+                });
+                return false;
+            });
+            return false;
+        },
+		icon: function(){
+			$("#sal-icon").on("click",function(){
+				swal({
+					title: "Sweet!",
+					text: "Here's a custom image.",
+					imageUrl: "assets/images/avatar.png",
+					confirmButtonColor: '#304ffe',
+				});
+				return false;
+			});
+			return false;
+		},
+		html: function(){
+			$("#sal-html").on("click",function(){
+				swal({
+					title: "HTML <small>Title</small>!",
+					text: 'A custom <span style="color:#F8BB86">html<span> message.',
+					html: true ,
+					confirmButtonColor: '#304ffe',
+				});
+				return false;
+			});
+			return false;
+		},
+		auto: function(){
+			$("#sal-auto").on("click",function(){
+				swal({
+					title: "Auto close alert!",
+					text: "I will close in 2 seconds.",
+					timer: 2000,
+					showConfirmButton: false
+				});
+				return false;
+			});
+			return false;
+		},
+		prompt: function(){
+			$("#sal-prompt").on("click",function(){
+				swal({
+					title: "An input!",
+					text: "Write something interesting:",
+					type: "input",   showCancelButton: true,
+					closeOnConfirm: false,
+					animation: "slide-from-top",
+					inputPlaceholder: "Write something" ,
+					confirmButtonColor: '#304ffe',
+				}, function(inputValue){
+					if (inputValue === false) return false;
+					if (inputValue === "") {
+						swal({
+							title: "You need to write something!",
+							type: 'error' ,
+							confirmButtonColor: '#f60e0e',
+						});
+						return false
+					}
+					swal({
+							title: "Nice!",
+							text: "You wrote: " + inputValue,
+							type: 'success' ,
+							confirmButtonColor: '#304ffe',
+						});
+				});
+				return false;
+			});
+			return false;
+		},
+	}
+
+
+})(jQuery);
---
+++ resources/views/iadelerigoruntule.blade.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Home - Papel Trade</title>
+
+    <!-- Main Styles -->
+    <link rel="stylesheet" href="assets/styles/style.min.css">
+
+    <!-- Material Design Icon -->
+    <link rel="stylesheet" href="assets/fonts/material-design/css/materialdesignicons.css">
+
+    <!-- mCustomScrollbar -->
+    <link rel="stylesheet" href="assets/plugin/mCustomScrollbar/jquery.mCustomScrollbar.min.css">
+
+    <!-- Waves Effect -->
+    <link rel="stylesheet" href="assets/plugin/waves/waves.min.css">
+
+    <!-- Sweet Alert -->
+    <link rel="stylesheet" href="assets/plugin/sweet-alert/sweetalert.css">
+
+    <!-- Percent Circle -->
+    <link rel="stylesheet" href="assets/plugin/percircle/css/percircle.css">
+
+    <!-- Chartist Chart -->
+    <link rel="stylesheet" href="assets/plugin/chart/chartist/chartist.min.css">
+
+    <!-- FullCalendar -->
+    <link rel="stylesheet" href="assets/plugin/fullcalendar/fullcalendar.min.css">
+    <link rel="stylesheet" href="assets/plugin/fullcalendar/fullcalendar.print.css" media='print'>
+
+    <!-- Color Picker -->
+    <link rel="stylesheet" href="assets/color-switcher/color-switcher.min.css">
+</head>
+
+<body>
+<div class="main-menu">
+    <header class="header">
+        <a href="index" class="logo">Papel Trade</a>
+        <button type="button" class="button-close fa fa-times js__menu_close"></button>
+    </header>
+    <!-- /.header -->
+    <div class="content">
+
+        <div class="navigation">
+            <ul class="menu js__accordion">
+                <li class="current">
+                    <a class="waves-effect" href=""><i class="menu-icon mdi mdi-view-dashboard"></i><span>Hesap Özeti</span></a>
+                </li>
+                <li>
+                    <a class="waves-effect parent-item js__control" href="#"><i class="menu-icon mdi mdi-flower"></i><span>Ürünler</span><span class="menu-arrow fa fa-angle-down"></span></a>
+                    <ul class="sub-menu js__content">
+                        <li><a href="icons-font-awesome-icons.html">Ürün Listeleme</a></li>
+                        <li><a href="icons-fontello.html">Satıştaki Ürünler</a></li>
+                        <!-- /.sub-menu js__content -->
+                        </li>
+                        <li>
+                            <a class="waves-effect" href="calendar.html"><i class="menu-icon mdi mdi-calendar-multiple"></i><span>Siparişler</span><span class="notice notice-danger">New</span></a>
+                        </li>
+                        <li>
+                            <a class="waves-effect parent-item js__control" href="#"><i class="menu-icon mdi mdi-chart-bar"></i><span>Tahsilatlar</span><span class="menu-arrow fa fa-angle-down"></span></a>
+                        </li>
+                        <li>
+                            <a class="waves-effect parent-item js__control" href="#"><i class="menu-icon mdi mdi-cube-outline"></i><span>İadeler</span><span class="menu-arrow fa fa-angle-down"></span></a>
+                            <ul class="sub-menu js__content">
+                                <li><a href="ui-notification.html">İade Oluştur</a></li>
+                                <li><a href="profile.html">İadeleri Görüntüle</a></li>
+                            </ul>
+                            <!-- /.sub-menu js__content -->
+                        </li>
+                        <li>
+                            <a class="waves-effect parent-item js__control" href="#"><i class="menu-icon mdi mdi-buffer"></i><span>İndirimler</span><span class="menu-arrow fa fa-angle-down"></span></a>
+                            <ul class="sub-menu js__content">
+                                <li><a href="ui-buttons.html">Otomatik İndirimler</a></li>
+                                <li><a href="ui-cards.html">İndirim Kodları</a></li>
+                            </ul>
+                            <!-- /.sub-menu js__content -->
+                        </li>
+                        <li>
+                            <a class="waves-effect parent-item js__control" href="#"><i class="menu-icon mdi mdi-email-outline"></i><span>Dükkan Yönetimi</span><span class="menu-arrow fa fa-angle-down"></a>
+                            <ul class="sub-menu js__content">
+                                <li><a href="form-elements.html">Dükkan Seçenekleri</a></li>
+                                <li><a href="form-advanced.html">Logo & Banner</a></li>
+                                <li><a href="form-fileupload.html">Kategori Ayarları</a></li>
+                                <li><a href="form-validation.html">Varyasyon Ayarları</a></li>
+                                <li><a href="form-wizard.html">Kargo Ayarları</a></li>
+                            </ul>
+                            <!-- /.sub-menu js__content -->
+                        </li>
+                        <li>
+                            <a class="waves-effect parent-item js__control" href="#"><i class="menu-icon mdi mdi-pencil-box"></i><span>Entegrasyonlar</span><span class="menu-arrow fa fa-angle-down"></span></a>
+                            <ul class="sub-menu js__content">
+                                <li><a href="form-elements.html">Modül Yönetimi</a></li>
+                                <li><a href="form-advanced.html">Sipariş Bildirimi</a></li>
+                                <li><a href="form-fileupload.html">Instagram Bağlantısı</a></li>
+                                <li><a href="form-validation.html">Facebook Bağlantısı</a></li>
+                            </ul>
+                            <!-- /.sub-menu js__content -->
+                        </li>
+                        <li>
+                            <a class="waves-effect parent-item js__control" href="#"><i class="menu-icon mdi mdi-table"></i><span>Hesap Yönetimi</span><span class="menu-arrow fa fa-angle-down"></span></a>
+                            <ul class="sub-menu js__content">
+                                <li><a href="tables-basic.html">Hesap Bilgileri</a></li>
+                                <li><a href="tables-datatable.html">Hesap Güvenliği</a></li>
+                            </ul>
+                            <!-- /.sub-menu js__content -->
+                        </li>
+
+        </div>
+        <!-- /.navigation -->
+    </div>
+    <!-- /.content -->
+</div>
+<!-- /.main-menu -->
+
+<div class="fixed-navbar">
+    <div class="pull-left">
+        <button type="button" class="menu-mobile-button glyphicon glyphicon-menu-hamburger js__menu_mobile"></button>
+        <h1 class="page-title">Home</h1>
+        <!-- /.page-title -->
+    </div>
+    <!-- /.pull-left -->
+    <div class="pull-right">
+        <div class="ico-item">
+            <a href="#" class="ico-item fa fa-search js__toggle_open" data-target="#searchform-header"></a>
+            <form action="#" id="searchform-header" class="searchform js__toggle"><input type="search" placeholder="Search..." class="input-search"><button class="fa fa-search button-search" type="submit"></button></form>
+            <!-- /.searchform -->
+        </div>
+        <div class="ico-item">
+            <img src="http://placehold.it/80x80" alt="" class="ico-img">
+            <ul class="sub-ico-item">
+                <li><a href="#">Dükkanım</a></li>
+                <li><a href="#">Ücretlendirme Tablosu</a></li>
+                <li><a href="#">Yardım</a></li>
+                <li><a class="js__logout" href="#">Log Out</a></li>
+            </ul>
+            <!-- /.sub-ico-item -->
+        </div>
+        <!-- /.ico-item -->
+    </div>
+    <!-- /.pull-right -->
+</div>
+<!-- /.fixed-navbar -->
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+<script src="assets/script/html5shiv.min.js"></script>
+<script src="assets/script/respond.min.js"></script>
+<![endif]-->
+<!--
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="assets/scripts/jquery.min.js"></script>
+<script src="assets/scripts/modernizr.min.js"></script>
+<script src="assets/plugin/bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/plugin/mCustomScrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
+<script src="assets/plugin/nprogress/nprogress.js"></script>
+<script src="assets/plugin/sweet-alert/sweetalert.min.js"></script>
+<script src="assets/plugin/waves/waves.min.js"></script>
+<!-- Full Screen Plugin -->
+<script src="assets/plugin/fullscreen/jquery.fullscreen-min.js"></script>
+
+<!-- Google Chart -->
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
+<!-- chart.js Chart -->
+<script src="assets/plugin/chart/chartjs/Chart.bundle.min.js"></script>
+<script src="assets/scripts/chart.chartjs.init.min.js"></script>
+
+<!-- FullCalendar -->
+<script src="assets/plugin/moment/moment.js"></script>
+<script src="assets/plugin/fullcalendar/fullcalendar.min.js"></script>
+<script src="assets/scripts/fullcalendar.init.js"></script>
+
+<!-- Sparkline Chart -->
+<script src="assets/plugin/chart/sparkline/jquery.sparkline.min.js"></script>
+<script src="assets/scripts/chart.sparkline.init.min.js"></script>
+
+<script src="assets/scripts/main.min.js"></script>
+<script src="assets/color-switcher/color-switcher.min.js"></script>
+</body>
+</html>
